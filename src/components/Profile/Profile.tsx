b16d9f04387891915556408dd1abefa79a7717fd
Alexander Vasiliev <selado@ya.ru>
2021-07-27 02:49:16 +0500

    Настроен и стилизован компонент Profile

---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from './Profile.module.scss';
+import profilePic from '../../data/images/profile-picture.jpg';
+
+
+function Profile() {
+  return (
+    <div className={ styles.profile }>
+      <h1 className={ styles.title }>Профиль</h1>
+      <div className={ styles.aboutBlock }>
+        <picture>
+          <img src={ profilePic } alt='Фото профиля' className={ styles.profilePicture } />
+        </picture>
+        <ul className={ styles.list }>
+          <li className={ styles.listItem }><span className={ styles.titleText } >Логин: </span><span className={ styles.text } >Admin</span></li>
+          <li className={ styles.listItem }><span className={ styles.titleText } >Пароль: </span><span className={ styles.text } >12345</span></li>
+          <li className={ styles.listItem }><span className={ styles.titleText } >Доступ: </span><span className={ styles.text } >смотреть профиль, править новости</span></li>
+        </ul>
+
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
