bc9fb0cbaa7724a135a31c361039fe417ccfabab
Alexander Vasiliev <selado@ya.ru>
2021-08-07 14:50:06 +0500

    Исправлено получение и отображение данных профиля

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,22 +1,43 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import styles from './Profile.module.scss';
-import profilePic from '../../data/images/profile-picture.jpg';
+import { ProfilesData } from '../../data/profiles';
+
 
 
 function Profile() {
+  const profiles = useSelector((store: any) => store.profiles.profiles);
+
   return (
     <div className={ styles.profile }>
       <h1 className='mainTitle'>Профиль</h1>
-      <div className={ styles.aboutBlock }>
-        <picture>
-          <img src={ profilePic } alt='Фото профиля' className={ styles.profilePicture } />
-        </picture>
-        <ul className={ styles.list }>
-          <li className={ styles.listItem }><span className={ styles.titleText } >Логин: </span><span className={ styles.text } >Admin</span></li>
-          <li className={ styles.listItem }><span className={ styles.titleText } >Пароль: </span><span className={ styles.text } >12345</span></li>
-          <li className={ styles.listItem }><span className={ styles.titleText } >Доступ: </span><span className={ styles.text } >смотреть профиль, править новости</span></li>
-        </ul>
-      </div>
+
+      {
+        profiles.map((item: ProfilesData) => {
+          return (
+            <div className={ styles.aboutBlock }>
+              <picture>
+                <img src={ item.avatar } alt='Фото профиля' className={ styles.profilePicture }/>
+              </picture>
+              <ul className={ styles.list }>
+                <li className={ styles.listItem }>
+                  <span className={ styles.titleText }>Логин: </span>
+                  <span className={ styles.text }>{ item.login }</span>
+                </li>
+                <li className={ styles.listItem }>
+                  <span className={ styles.titleText }>Пароль: </span>
+                  <span className={ styles.text }>{ item.password }</span>
+                </li>
+                <li className={ styles.listItem }>
+                  <span className={ styles.titleText }>Доступ: </span>
+                  <span className={ styles.text }>{ item.access.join(', ') }</span>
+                </li>
+              </ul>
+            </div>
+          )
+        })
+      }
+
     </div>
   );
 }
